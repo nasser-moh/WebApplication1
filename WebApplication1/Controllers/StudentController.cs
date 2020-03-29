5249e3b540eb95c39d7150b4a6e6eb2e6ef951bd
Muhammad Nasser <mohamad.naser59@gmail.com>
2020-03-29 21:52:19 +0300

    Commmiiittteeeddd

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetStudentName()
+        {
+            Student student1 = new Student();
+            
+            student1.Id = 1117004;
+            student1.studentName = "Muhammad Nasser";
+            return View(student1);
+        }
+    }
+}
